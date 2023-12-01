d78e572933b88890ba338c67eaa0534b4a9c2ed1
Martin Zikmund <martin@zikmund.dev>
2023-12-01 10:31:14 +0100

    Day 1 (Part 1)

---
+++ Day01_1/Program.cs
@@ -0,0 +1,11 @@
+﻿long total = 0;
+while(Console.ReadLine() is { } line)
+{
+    int firstDigit = line.First(line => char.IsDigit(line)) - '0';
+    int lastDigit = line.Last(line => char.IsDigit(line)) - '0';
+
+    var fullNumber = firstDigit * 10 + lastDigit;
+    total += fullNumber;
+}
+
+Console.WriteLine(total);
