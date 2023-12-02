1824f7e28597b2f58110da07f66b6150c8f6af19
Martin Zikmund <martin@zikmund.dev>
2023-12-02 17:12:14 +0100

    Day 2 (Part 2)

---
+++ Day02_2/Program.cs
@@ -0,0 +1,38 @@
+﻿var runningTotal = 0;
+
+while (Console.ReadLine() is { } line)
+{
+    var gameInfo = line.Split(':');
+    var gameId = int.Parse(gameInfo[0].Split(' ')[1]);
+    var rounds = gameInfo[1].Split(';', StringSplitOptions.TrimEntries);
+    var maxRed = 0;
+    var maxGreen = 0;
+    var maxBlue = 0;
+    foreach (var round in rounds)
+    {
+        var colorInfos = round.Split(',', StringSplitOptions.TrimEntries);
+        foreach (var color in colorInfos)
+        {
+            var colorInfo = color.Split(' ');
+            var colorCount = int.Parse(colorInfo[0]);
+            var colorName = colorInfo[1];
+            switch (colorName)
+            {
+                case "red":
+                    maxRed = Math.Max(colorCount, maxRed);
+                    break;
+                case "green":
+                    maxGreen = Math.Max(colorCount, maxGreen);
+                    break;
+                case "blue":
+                    maxBlue = Math.Max(colorCount, maxBlue);
+                    break;
+            }
+        }
+    }
+
+    var product = maxRed * maxGreen * maxBlue;
+    runningTotal += product;
+}
+
+Console.WriteLine(runningTotal);
