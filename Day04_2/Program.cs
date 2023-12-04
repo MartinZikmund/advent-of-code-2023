4e8dfc58a8733477b68748e93b64a0e1d00c3ef9
Martin Zikmund <martin@zikmund.dev>
2023-12-04 13:22:38 +0100

    Day 4 (Part 2)

---
+++ Day04_2/Program.cs
@@ -0,0 +1,32 @@
+﻿using Tools;
+
+var input = InputTools.ReadAllLines();
+int[] cardCount = new int[input.Length];
+for (int i = 0; i < cardCount.Length; i++)
+{
+    cardCount[i] = 1;
+}
+
+for (int cardId = 0; cardId < input.Length; cardId++)
+{
+    string? line = input[cardId];
+    var parts = line.Split(':');
+    var numbers = parts[1].Split('|');
+    var pickedNumbers = numbers[0]
+        .Split(' ', StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries)
+        .Select(int.Parse)
+        .ToArray();
+    var ourNumbers = numbers[1]
+        .Split(' ', StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries)
+        .Select(int.Parse)
+        .ToArray();
+
+    var matchCount = pickedNumbers.Intersect(ourNumbers).Count();
+
+    for (int i = 0; i < matchCount; i++)
+    {
+        cardCount[cardId + 1 + i] += cardCount[cardId];
+    }
+}
+
+Console.WriteLine(cardCount.Sum());
--- Tools/InputTools.cs
+++ Tools/InputTools.cs
@@ -2,14 +2,5 @@
 
 public static class InputTools
 {
-    public static string[] ReadAllLines()
-    {
-        var lines = new List<string>();
-        while (Console.ReadLine() is string line)
-        {
-            lines.Add(line);
-        }
-
-        return lines.ToArray();
-    }
+    public static string[] ReadAllLines() => File.ReadAllLines("input.txt");
 }
