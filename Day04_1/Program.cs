9d91270bdf532a29082809e812be2f6b6ed6abf1
Martin Zikmund <martin@zikmund.dev>
2023-12-04 13:22:28 +0100

    Day 4 (Part 1)

---
+++ Day04_1/Program.cs
@@ -0,0 +1,28 @@
+﻿using Tools;
+
+long runningTotal = 0;
+
+var input = InputTools.ReadAllLines();
+foreach(var line in input)
+{
+    var parts = line.Split(':');
+    var numbers = parts[1].Split('|');
+    var pickedNumbers = numbers[0]
+        .Split(' ', StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries)
+        .Select(int.Parse)
+        .ToArray();
+    var ourNumbers = numbers[1]
+        .Split(' ', StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries)
+        .Select(int.Parse)
+        .ToArray();
+
+    var matchCount = pickedNumbers.Intersect(ourNumbers).Count();
+    if (matchCount == 0)
+    {
+        continue;
+    }
+
+    runningTotal += (1 << (matchCount - 1));
+}
+
+Console.WriteLine(runningTotal);
