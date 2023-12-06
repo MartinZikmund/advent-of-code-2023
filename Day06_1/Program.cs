b8733ff898a0cb5ed0818efa8aa14e8af48250a8
Martin Zikmund <martin@zikmund.dev>
2023-12-06 09:34:35 -0500

    Day 6 (Part 1)

---
+++ Day06_1/Program.cs
@@ -0,0 +1,34 @@
+﻿var input = File.ReadAllLines("input.txt");
+
+var times = input[0]
+        .Substring("Time:".Length)
+        .Split(" ", StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries)
+        .Select(int.Parse)
+        .ToArray();
+
+var records = input[1]
+        .Substring("Distance:".Length)
+        .Split(" ", StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries)
+        .Select(int.Parse)
+        .ToArray();
+
+var total = 1;
+
+for (int race = 0; race < times.Length; race++)
+{
+    var waysToWin = 0;
+    var totalTime = times[race];
+    var record = records[race];
+
+    for (int time = 0; time < totalTime; time++)
+    {
+        if (time * (totalTime - time) > record)
+        {
+            waysToWin++;
+        }
+    }
+
+    total *= waysToWin;
+}
+
+Console.WriteLine(total);
