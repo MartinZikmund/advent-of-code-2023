977f5adef4dc73b922f9afab4fbeb9c7f1b58afa
Martin Zikmund <martin@zikmund.dev>
2023-12-10 20:10:38 -0500

    Day 9 (Part 2)

---
+++ Day09_2/Program.cs
@@ -0,0 +1,51 @@
+﻿var input = File.ReadAllLines("input.txt");
+
+long runningTotal = 0;
+
+foreach (var line in input)
+{
+    var sequence = line.Split(' ').Select(int.Parse).ToArray();
+    runningTotal += GetExtrapolatedValue(sequence);
+}
+
+Console.WriteLine(runningTotal);
+
+long GetExtrapolatedValue(int[] sequence)
+{
+    int[,] chart = new int[sequence.Length + 1, sequence.Length + 1];
+
+    var allZeroes = false;
+    for (int i = 0; i < sequence.Length; i++)
+    {
+        chart[0, i] = sequence[i];
+    }
+
+    int chartRow = 0;
+    while (!allZeroes)
+    {
+        allZeroes = true;
+        for (int i = 0; i < sequence.Length - chartRow - 1; i++)
+        {
+            var leftValue = chart[chartRow, i];
+            var rightValue = chart[chartRow, i + 1];
+            var diff = rightValue - leftValue;
+            chart[chartRow + 1, i] = diff;
+            if (diff != 0)
+            {
+                allZeroes = false;
+            }
+        }
+
+        chartRow++;
+    }
+
+    int currentValue = 0;
+    for (int row = chartRow; row > 0; row--)
+    {
+        var difference = currentValue;
+        var valueAbove = chart[row - 1, 0];
+        currentValue = valueAbove - difference;
+    }
+
+    return currentValue;
+}
