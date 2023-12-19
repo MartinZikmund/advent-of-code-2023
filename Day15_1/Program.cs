91b232e6f085a6d123222ea8d675e42ac5c550c1
Martin Zikmund <martin@zikmund.dev>
2023-12-19 10:56:18 +0100

    Day 15 (Part 1)

---
+++ Day15_1/Program.cs
@@ -0,0 +1,25 @@
+﻿var input = File.ReadAllText("input.txt");
+
+var parts = input.Split(",", StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries);
+
+var runningTotal = 0;
+
+foreach(var part in parts)
+{
+    runningTotal += Hash(part);
+}
+
+Console.WriteLine(runningTotal);
+
+int Hash(string input)
+{
+    var value = 0;
+    foreach (var c in input)
+    {
+        value += c;
+        value *= 17;
+        value %= 256;
+    }
+
+    return value;
+}
