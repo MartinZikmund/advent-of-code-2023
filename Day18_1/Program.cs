e0b125ecae2160581a250638ac0296d1ef409707
Martin Zikmund <martin@zikmund.dev>
2024-01-17 19:21:18 +0100

    Day 18

---
+++ Day18_1/Program.cs
@@ -0,0 +1,50 @@
+﻿using Tools;
+
+var input = File.ReadAllLines("input.txt");
+var origin = new Point();
+var currentPosition = origin;
+var points = new List<Point>();
+var totalBoundaryPoints = 0L;
+foreach (var line in input)
+{
+    var parts = line.Split(' ');
+    var direction = parts[0];
+    var distance = int.Parse(parts[1]);
+    Point newPosition = currentPosition;
+    switch (direction)
+    {
+        case "R":
+            newPosition = new Point(currentPosition.X + distance, currentPosition.Y);
+            break;
+        case "L":
+            newPosition = new Point(currentPosition.X - distance, currentPosition.Y);
+            break;
+        case "U":
+            newPosition = new Point(currentPosition.X, currentPosition.Y - distance);
+            break;
+        case "D":
+            newPosition = new Point(currentPosition.X, currentPosition.Y + distance);
+            break;
+        default:
+            throw new InvalidOperationException();
+    }
+    points.Add(newPosition);
+    totalBoundaryPoints += Math.Abs(newPosition.X - currentPosition.X) + Math.Abs(newPosition.Y - currentPosition.Y);
+    currentPosition = newPosition;
+}
+
+// Calculate area using Shoelace formula
+long area = 0;
+for (var currentPoint = 0; currentPoint < points.Count - 1; currentPoint++)
+{
+    var nextPoint = currentPoint + 1 == points.Count ? 0 : currentPoint + 1;
+    area += points[currentPoint].X * points[nextPoint].Y - points[nextPoint].X * points[currentPoint].Y;
+}
+area = Math.Abs(area) / 2;
+
+// Use Pick's theorem to calculate the number of points inside the polygon
+var pointsInside = area - totalBoundaryPoints / 2 + 1;
+Console.WriteLine($"Points inside: {pointsInside}");
+Console.WriteLine($"Area: {area}");
+
+Console.WriteLine("Total points: " + (totalBoundaryPoints + pointsInside));
---
+++ Day18_2/Program.cs
@@ -0,0 +1,51 @@
+﻿var input = File.ReadAllLines("input.txt");
+var origin = new Point(0, 0);
+var currentPosition = origin;
+var points = new List<Point>();
+var totalBoundaryPoints = 0L;
+foreach (var line in input)
+{
+    var parts = line.Split(' ');
+    var encodedInstruction = parts[2].Trim('(').Trim(')').Trim('#');
+    var direction = encodedInstruction.Last() - '0';
+    var distance = Convert.ToInt32(encodedInstruction.Substring(0, encodedInstruction.Length - 1), 16);
+    var newPosition = currentPosition;
+    switch (direction)
+    {
+        case 0:
+            newPosition = new Point(currentPosition.X + distance, currentPosition.Y);
+            break;
+        case 1:
+            newPosition = new Point(currentPosition.X, currentPosition.Y + distance);
+            break;
+        case 2:
+            newPosition = new Point(currentPosition.X - distance, currentPosition.Y);
+            break;
+        case 3:
+            newPosition = new Point(currentPosition.X, currentPosition.Y - distance);
+            break;
+        default:
+            throw new InvalidOperationException();
+    }
+    points.Add(newPosition);
+    totalBoundaryPoints += Math.Abs(newPosition.X - currentPosition.X) + Math.Abs(newPosition.Y - currentPosition.Y);
+    currentPosition = newPosition;
+}
+
+// Calculate area using Shoelace formula
+long area = 0;
+for (var currentPoint = 0; currentPoint < points.Count - 1; currentPoint++)
+{
+    var nextPoint = currentPoint + 1 == points.Count ? 0 : currentPoint + 1;
+    area += points[currentPoint].X * points[nextPoint].Y - points[nextPoint].X * points[currentPoint].Y;
+}
+area = Math.Abs(area) / 2;
+
+// Use Pick's theorem to calculate the number of points inside the polygon
+var pointsInside = area - totalBoundaryPoints / 2 + 1;
+Console.WriteLine($"Points inside: {pointsInside}");
+Console.WriteLine($"Area: {area}");
+
+Console.WriteLine("Total points: " + (totalBoundaryPoints + pointsInside));
+
+record Point(long X, long Y);
