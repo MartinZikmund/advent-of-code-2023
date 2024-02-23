864297ff377335be7e1cf71702c8b749e48369c0
Martin Zikmund <martin@zikmund.dev>
2024-02-23 13:13:43 +0100

    Day 22

---
+++ Day22_1/Program.cs
@@ -0,0 +1,92 @@
+﻿using Day22_1;
+
+var input = File.ReadAllLines("input.txt");
+
+var bricks = new List<Brick>();
+
+var id = 0;
+foreach (var line in input)
+{
+    var parts = line.Split(new[] {',','~'}, StringSplitOptions.RemoveEmptyEntries);
+    var x1 = int.Parse(parts[0]);
+    var y1 = int.Parse(parts[1]);
+    var z1 = int.Parse(parts[2]);
+    var x2 = int.Parse(parts[3]);
+    var y2 = int.Parse(parts[4]);
+    var z2 = int.Parse(parts[5]);
+    var brick = new Brick(id, x1, y1, z1, x2, y2, z2);
+    bricks.Add(brick);
+    id++;
+}
+
+var sortedBricks = bricks.OrderBy(b => b.MinZ).ToList();
+for (var currentBrickId = 0; currentBrickId < sortedBricks.Count; currentBrickId++)
+{
+    var currentBrick = sortedBricks[currentBrickId];
+    var currentMinZ = 1;
+    for (int brickToCheckId = 0; brickToCheckId < currentBrickId; brickToCheckId++)
+    {
+        var brickToCheck = sortedBricks[brickToCheckId];
+        if (brickToCheck.Intersects(currentBrick))
+        {
+            currentMinZ = Math.Max(currentMinZ, brickToCheck.MaxZ + 1);
+        }
+    }
+
+    var deltaZ = currentMinZ - currentBrick.MinZ;
+
+    currentBrick.Z1 += deltaZ;
+    currentBrick.Z2 += deltaZ;
+}
+
+var supportedBy = new Dictionary<Brick, HashSet<Brick>>();
+var supports = new Dictionary<Brick, HashSet<Brick>>();
+foreach (var brick in bricks)
+{
+    supportedBy.Add(brick, new());
+    supports.Add(brick, new());
+}
+sortedBricks = bricks.OrderBy(b => b.MinZ).ToList();
+
+for (var currentBrickId = 0; currentBrickId < sortedBricks.Count; currentBrickId++)
+{
+    var currentBrick = sortedBricks[currentBrickId];
+    for (int brickToCheckId = 0; brickToCheckId < currentBrickId; brickToCheckId++)
+    {
+        var brickToCheck = sortedBricks[brickToCheckId];
+        if (brickToCheck.Intersects(currentBrick) &&
+            brickToCheck.MaxZ + 1 == currentBrick.MinZ)
+        {
+            supportedBy[currentBrick].Add(brickToCheck);
+            supports[brickToCheck].Add(currentBrick);
+        }
+    }
+}
+
+var safeToDisintegrate = 0;
+for (var currentBrickId = 0; currentBrickId < bricks.Count; currentBrickId++)
+{
+    var currentBrick = bricks[currentBrickId];
+    var isSafe = true;
+    foreach (var supportedBrick in supports[currentBrick])
+    {
+        if (supportedBy[supportedBrick].Count == 1)
+        {
+            isSafe = false;
+            break;
+        }
+    }
+
+    if (isSafe)
+    {
+        safeToDisintegrate++;
+    }
+}
+
+for (var currentBrickId = 0; currentBrickId < bricks.Count; currentBrickId++)
+{
+    var currentBrick = bricks[currentBrickId];
+    Console.WriteLine($"{currentBrick.Id} is supporting {string.Join(", ", supports[currentBrick].Select(b => b.Id.ToString()))}");
+}
+
+Console.WriteLine(safeToDisintegrate);
---
+++ Day22_2/Brick.cs
@@ -0,0 +1,41 @@
+﻿using System.Drawing;
+
+namespace Day22_1;
+
+internal class Brick
+{
+    public Brick(int id, int x1, int y1, int z1, int x2, int y2, int z2)
+    {
+        Id = id;
+        X1 = x1;
+        Y1 = y1;
+        Z1 = z1;
+        X2 = x2;
+        Y2 = y2;
+        Z2 = z2;
+    }
+
+    public int Id { get; }
+    public int X1 { get; set; }
+    public int Y1 { get; set; }
+    public int Z1 { get; set; }
+    public int X2 { get; set; }
+    public int Y2 { get; set; }    
+    public int Z2 { get; set; }
+
+    public int MinZ => Math.Min(Z1, Z2);
+
+    public int MaxZ => Math.Max(Z1, Z2);
+
+    public bool Intersects(Brick other)
+    {
+        var rect1 = new Rectangle(X1, Y1, X2 - X1 + 1, Y2 - Y1 + 1);
+        var rect2 = new Rectangle(other.X1, other.Y1, other.X2 - other.X1 + 1, other.Y2 - other.Y1 + 1);
+        return rect1.IntersectsWith(rect2);
+    }
+
+    public override string ToString()
+    {
+        return $"({X1},{Y1},{Z1})-({X2},{Y2},{Z2})";
+    }
+}
